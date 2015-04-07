d59f31600c668a1582031e2d3b379b63a79bc19a
jeje2208 <baziinje@gmail.com>
2015-04-07 18:43:39 +0200

    Mise en place de l'objet Grade

--- Lobby/src/object/Grade.java
+++ Lobby/src/object/Grade.java
@@ -1,5 +1,7 @@
 package object;
 
+import java.util.ArrayList;
+
 public class Grade {
 	
 	private String name;
@@ -7,9 +9,50 @@ public class Grade {
 	private String prexife;
 	private int multiplicateur;
 	
+	public static ArrayList<Grade> gradeList;
+	
 	
 	public Grade(){
 		
 	}
+	
+	public Grade getGrade(String name){
+		for(Grade g : gradeList){
+			if(g.name.equalsIgnoreCase(name))
+					return g;
+		}
+		return null;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+
+	public String getPrexife() {
+		return prexife;
+	}
+
+
+	public void setPrexife(String prexife) {
+		this.prexife = prexife;
+	}
+
+
+	public int getMultiplicateur() {
+		return multiplicateur;
+	}
+
+
+	public void setMultiplicateur(int multiplicateur) {
+		this.multiplicateur = multiplicateur;
+	}
+	
+	
 
 }
