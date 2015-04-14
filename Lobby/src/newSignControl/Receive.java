4639c44de1db656321486f40463f422b3a57bd6a
jeje2208 <baziinje@gmail.com>
2015-04-14 21:26:02 +0200

    s

--- Lobby/src/newSignControl/Receive.java
+++ Lobby/src/newSignControl/Receive.java
@@ -7,6 +7,12 @@ public class Receive {
 	public Receive(String server, String label, String message){
 		if(label.equalsIgnoreCase("sign")){
 			Panneau p = object.Panneau.getPanneau(server);
+			String[] line = message.split(" ");
+			String line1 = line[0];
+			String line2 = line[1];
+			String line3 = line[2];
+			String line4 = line[3];
+			p.setSign(line1, line2, line3, line4);
 		}
 	}
 
