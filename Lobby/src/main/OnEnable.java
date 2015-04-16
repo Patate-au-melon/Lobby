bc3431b95d9aa5d358f6ea0dd91b67150f704e4e
jeje2208 <baziinje@gmail.com>
2015-04-16 19:27:19 +0200

    dernier debug

--- Lobby/src/main/OnEnable.java
+++ Lobby/src/main/OnEnable.java
@@ -1,12 +1,17 @@
 package main;
 
+import java.util.ArrayList;
+
+import object.Panneau;
+
 import org.bukkit.Bukkit;
 
 public class OnEnable {
 	
 	public OnEnable(){
-		new signControl.Startup();
+		object.Panneau.listSign = new ArrayList<Panneau>();
 		ApiBaseDeDonnee.connect();
+		new signControl.Startup();
 		Config.createConfig();
 		new server.ServerMain().runTaskAsynchronously(Main.getPlugin());
 		Bukkit.getLogger().info("Plugin actif");
