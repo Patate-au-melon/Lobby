d3e11afcce5f9581dbf3b7d2a218351da510e22b
jeje2208 <baziinje@gmail.com>
2015-04-16 19:46:33 +0200

    dggfgdfgdfgdebug

--- Lobby/src/server/Transmit.java
+++ Lobby/src/server/Transmit.java
@@ -3,7 +3,6 @@ package server;
 public class Transmit {
 	
 	public Transmit(String server, String label, String message){
-		System.out.println(server + label + message);
 		new signControl.Receive(server, label, message);
 	}
 
