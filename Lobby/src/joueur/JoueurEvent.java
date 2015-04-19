64cabab058da8491fc6e70e0543210deafdd989b
jeje2208 <baziinje@gmail.com>
2015-04-19 13:23:59 +0200

    Ajout de l'event pour ne plus prendre de degat

--- Lobby/src/joueur/JoueurEvent.java
+++ Lobby/src/joueur/JoueurEvent.java
@@ -1,27 +1,22 @@
 package joueur;
 
-import object.Joueur;
-
-import org.bukkit.entity.Player;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoueurEvent implements Listener{
 	
 	@EventHandler
 	public void playerJoin(PlayerJoinEvent e){
-		Player p = e.getPlayer();
-		if(object.Joueur.joueurList.contains(p.getName())){
-			Joueur j = object.Joueur.getJoueur(p.getName());
-			Joueur jo = object.Joueur.getJoueur(p.getUniqueId());
-			if(j.equals(jo)){
-				if(j.getServer().equalsIgnoreCase("Lobby")){
-					p.sendMessage("Rebonjour "+p.getName());
-				}else{
-					
-				}
-			}
+
+	}
+	
+	@EventHandler
+	public void playerJoin(EntityDamageEvent e){
+		if(e.getEntityType() == EntityType.PLAYER){
+			e.setCancelled(true);
 		}
 	}
 
