6d6c71552cb69d14fcae8e2e64492ecdd116901e
jeje2208 <baziinje@gmail.com>
2015-05-27 21:09:58 +0200

    A chaque message on precise que le serveur est connecte avec le serveur

--- src/signControl/Message.java
+++ src/signControl/Message.java
@@ -9,6 +9,7 @@ public class Message {
 	public static void receive(String server, ArrayList<String> message){
 		if(message.get(0).equalsIgnoreCase("signControl Update")){  //Verification que l'on a le bon message
 			Panneau pan = Panneau.getPanneau(server);  //On recupere le panneau
+			pan.setServerState(true);
 			pan.setLine(message.get(1), message.get(2), message.get(3), message.get(4)); //On modifie le texte sur le panneau en fonction du message
 		}
 	}
