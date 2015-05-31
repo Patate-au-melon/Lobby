7df3504cff8ea583749a2a1b73ddad86d725d305
jeje2208 <baziinje@gmail.com>
2015-05-31 21:32:07 +0200

    Ajout
    Ajout du debut de l'objet Joueur

---
+++ src/joueur/Joueur.java
@@ -0,0 +1,19 @@
+package joueur;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import main.Grade;
+
+public class Joueur {
+	
+	public static ArrayList<Joueur> listJoueur;
+	
+	private String name;
+	private UUID id;
+	
+	private int money;
+	
+	private Grade grade;
+
+}
